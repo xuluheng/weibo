d5fdb1c7e0ea09702b11538a908e9e81b54829c8
xuluheng <xuluheng634@163.com>
2018-12-28 18:19:51 +0800

    切割头部和尾部子试图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>@yield('title', 'Weibo App') - 专注建站</title>
-  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-</head>
-<body>
+  <head>
+    <title>@yield('title', 'Weibo App') - 专注建站</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  </head>
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <body>
+    @include('layouts._header')
+
     <div class="container">
-      <a class="navbar-brand" href="/">Weibo App</a>
-      <ul class="navbar-nav justify-content-end">
-        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">登陆</a></li>
-      </ul>
-    </div>
-  </nav>
 
-  <div class="container">
-    @yield('content')
-  </div>
-</body>
+      <div class="offset-md-1 col-md-10">
+
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+
+    </div>
+
+  </body>
+
 </html>
