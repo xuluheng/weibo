ea266824f1c8ea92aae45bae2b2dc6bd163bcbac
xuluheng <xuluheng634@163.com>
2018-12-29 16:54:07 +0000

    创建会话

--- config/services.php
+++ config/services.php
@@ -31,7 +31,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => App\Models::class,
+        'model' => App\Models\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
         'webhook' => [
