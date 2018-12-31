f9d0636a63b93d4323f51cc79f5a077d73fc2170
xuluheng <xuluheng634@163.com>
2018-12-31 14:05:29 +0000

    增加管理员权限

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'xuluheng';
         $user->email = 'xuluheng634@163.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
